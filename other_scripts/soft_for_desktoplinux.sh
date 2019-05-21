93066f8d623d1676234fdcf61bc99bb016117784
Nikita Kalitin <nikita.s.kalitin@gmail.com>
2019-05-21 18:25:44 +0300

    change soft_fro_desktop.....

---
+++ other_scripts/soft_for_desktoplinux.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#apt-get install virtualbox
+apt-get install terminator -y
+apt-get install nodejs -y
+apt-get install php-7 -y
+apt-get install ssh -y
+apt-get install chrome -y
+apt-get install mariadb -y
+apt-get install apache2 -y
+apt-get install git -y
+apt-get install shutter -y
+apt-get install vokoscreen -y
+apt-get install gimp -y
+apt-get isnstall inkscape -y
+
