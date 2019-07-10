9975362aa0e7be3426bc308dca6d14205dec3db3
Nikita Kalitin <nikita.s.kalitin@gmail.com>
2019-07-10 11:52:33 +0300

    add solution for convert from odt to pdf

---
+++ other_scripts/unoconv/example.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Sample use of Unoconv class
+ *  
+ */
+require 'Unoconv.php';
+ 
+use Unoconv\Unoconv;
+ 
+// Converting to PDF
+//$originFilePath = 'test.odt';
+//$outputDirPath  = './';
+//Unoconv::convertToPdf($originFilePath, $outputDirPath);
+ 
+// Converting to DOCX
+//$originFilePath = 'test.odt';
+//$outputDirPath  = './';
+//Unoconv::convert($originFilePath, $outputDirPath, 'docx');
+
+function recursive($dir,$outdir)
+{
+   static $deep = 0;
+   static $countDirs = 0;
+   static $countFiles = 0;
+ 
+   $odir = opendir($dir);
+ 
+   while (($file = readdir($odir)) !== FALSE)
+   {
+      if ($file == '.' || $file == '..')
+      {
+         continue;
+      }
+      else
+      {
+		 if(!is_dir($dir.DIRECTORY_SEPARATOR.$file)){ 
+			Unoconv::convertToPdf($dir.DIRECTORY_SEPARATOR.$file, $outdir.DIRECTORY_SEPARATOR.$file); 
+			$countFiles++;
+		}
+         echo str_repeat('---', $deep).$dir.DIRECTORY_SEPARATOR.$file.'<br>';
+         //echo str_repeat('---', $deep).$file.'<br>';
+      }
+ 
+      if (is_dir($dir.DIRECTORY_SEPARATOR.$file))
+      {
+		 $countDirs++; 
+         $deep ++;
+         $current_dir_name=$file;
+         recursive($dir.DIRECTORY_SEPARATOR.$file,$outdir.DIRECTORY_SEPARATOR.$current_dir_name);
+         $deep --;
+      }
+   }
+      closedir($odir);
+      return ['folders'=>$countDirs, 'files'=>$countFiles];
+}
+
+$dir='/home/kalitins/DEV/WORK_TMP/input';
+$output='/home/kalitins/DEV/WORK_TMP/output'; 
+$arr=recursive($dir,$output);
+echo "папок: ".$arr['folders']." файлов: ".$arr['files']." <br>";
