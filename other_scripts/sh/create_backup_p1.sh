a3a82e03f3a3b320616c46db157bca12c6e0159a
Nikita Kalitin <nikita.s.kalitin@gmail.com>
2019-08-23 17:39:08 +0300

    change format for backup

--- other_scripts/sh/create_backup_p1.sh
+++ other_scripts/sh/create_backup_p1.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-tar -cvpzf ~/DEV/BACKUPs/MNO/backup.mno.`date +%d-%m-%Y`.tar.gz --one-file-system /media/v/mno.users/kalitinns/
+tar -cvpzf ~/DEV/BACKUPs/MNO/backup.mno.`date +%d-%m-%Y_%H:%M`.tar.gz --one-file-system /media/v/mno.users/kalitinns/
